e788f58db79059f5766b8acbc4207cce63199168
patrickbindelli <patrickvenanbindelli@gmail.com>
2023-10-23 19:54:05 -0300

    feat: add radix tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import { createPlugin } from "windy-radix-palette";
+
+const colors = createPlugin();
+
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [colors.plugin],
 };
